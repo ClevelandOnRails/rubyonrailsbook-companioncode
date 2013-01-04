87368681c21d736f10fb2fd40e5117931580de34
James Gifford <james@jamesrgifford.com>
2013-01-04 10:47:35 -0500

    Initial commit of companion code, DemoApp with rails4 from --edge (git)

---
+++ DemoApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '6313f53cd5f58468f190ab8e447dbc0abd591ff40fe3eafe4181050fe641ce2a72e603fd9b76228775b7fd73d6cde233a16f26d6d00fd9ae658268cfdf8a1ac0'
