0abce9d30d1a8595da40706c9534cf2ada6a5d33
James Gifford <james@jamesrgifford.com>
2013-01-29 17:50:36 -0500

    Closes #4 on ClevelandRailsCourse/rubyonrailsbook

--- DemoApp/app/models/article.rb
+++ DemoApp/app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   validates :body, :presence => true
   validates :title, :presence => true,
                 :length => { :minimum => 5 }
