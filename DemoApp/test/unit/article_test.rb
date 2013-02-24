d7b949c4bec9dc9ec6417c58a5829b54ad8f561b
James Gifford <james@jamesrgifford.com>
2013-02-23 22:42:47 -0500

    Updated rails, curriculum is now even with everything else

--- DemoApp/test/unit/article_test.rb
+++ DemoApp/test/unit/article_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "without a body or title is is not valid" do
+    article = Article.new
+    assert !article.save
+  end
+
+  test "that with a body and title it is valid" do
+    article = Article.new(:title => "This is a title", :body => "This is a body")
+    assert article.save
+  end
+
 end
