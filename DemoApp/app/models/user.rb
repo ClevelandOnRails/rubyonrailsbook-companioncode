dff40ba827ee0de0b8f740697d778eef243911e4
James Gifford <james@jamesrgifford.com>
2013-04-04 12:18:27 -0400

    pausing for the afternoon

--- DemoApp/app/models/user.rb
+++ DemoApp/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
 end
